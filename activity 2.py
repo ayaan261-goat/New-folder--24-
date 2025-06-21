03faa37b2d45fd1b07b7dfe46fe46bb700affc5d
ayaan261-goat <ktm.ayaangupta@gmail.com>
2025-06-21 09:28:17 +0545

    hi

---
+++ activity 2.py
@@ -0,0 +1,9 @@
+import pygame
+
+pygame.init()
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+
+display_surface = pygame.display.set_mode((SCREEN WIDTH , SCREEN_HEIGHT))
+pygame.display.set_caption('adding image and backround image')
+background_
+                           
